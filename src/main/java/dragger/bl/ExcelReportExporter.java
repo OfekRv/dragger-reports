ffca47eb0ca1dc29773fb90be0ccbbf7dabc435d
OfekRv <ofekRv@gmail.com>
2018-09-28 23:10:34 +0300

    file download stub

--- src/main/java/dragger/bl/ExcelReportExporter.java
+++ src/main/java/dragger/bl/ExcelReportExporter.java
@@ -2,13 +2,23 @@ package dragger.bl;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import dragger.entities.Query;
 import dragger.entities.Report;
 
 @Named
 public class ExcelReportExporter implements ReportExporter {
+	@Inject
+	QueryGenerator generator;
+	@Inject
+	JdbcTemplate executer;
 
 	@Override
 	public File export(Report reportToExport) throws IOException {
@@ -17,4 +27,7 @@ public class ExcelReportExporter implements ReportExporter {
 		return new File("file1.xls");
 	}
 
+	private Collection<Map<String, Object>> executeQuery(Query query) {
+		return executer.queryForList(generator.generate(query));
+	}
 }
