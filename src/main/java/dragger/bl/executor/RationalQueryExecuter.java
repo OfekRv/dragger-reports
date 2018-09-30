7433b1433837d7d42933ec34b6f3751ca805e4e5
OfekRv <ofekRv@gmail.com>
2018-09-30 19:51:06 +0300

    set to read only

--- src/main/java/dragger/bl/executor/RationalQueryExecuter.java
+++ src/main/java/dragger/bl/executor/RationalQueryExecuter.java
@@ -1,15 +1,27 @@
 package dragger.bl.executor;
 
+import java.sql.SQLException;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import dragger.exceptions.DraggerExportException;
+
 @Named
 public class RationalQueryExecuter implements QueryExecutor {
 	@Inject
 	JdbcTemplate executer;
+
+	public RationalQueryExecuter() throws DraggerExportException {
+		try {
+			executer.getDataSource().getConnection().setReadOnly(true);
+		} catch (SQLException e) {
+			throw new DraggerExportException("Could not set connection to read only", e);
+		}
+	}
 
 	@Override
 	public SqlRowSet executeQuery(String query) {
