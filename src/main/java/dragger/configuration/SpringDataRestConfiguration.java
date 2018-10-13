62aefb4fe2eef390227ea666189b2218fe3a93c6
OfekRv <ofekRv@gmail.com>
2018-10-13 18:52:09 +0300

    exposed ids

---
+++ src/main/java/dragger/configuration/SpringDataRestConfiguration.java
@@ -0,0 +1,15 @@
+package dragger.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+import dragger.entities.Report;
+
+@Configuration
+public class SpringDataRestConfiguration extends RepositoryRestConfigurerAdapter {
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Report.class);
+	}
+}
