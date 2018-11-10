d65bbd5cdc02708210384d90a057ce36bfe989cf
OfekRv <ofekRv@gmail.com>
2018-11-10 17:44:34 +0200

    build report refactor

--- src/main/resources/static/dragger/buildReport/buildReport.js
+++ src/main/resources/static/dragger/buildReport/buildReport.js
@@ -1,22 +1,45 @@
-angular.module("dragger").controller(
-		"buildReportController",
-		function($scope, $http) {
-			$scope.models = {
-				selected : null,
-				lists : {
-					"Columns" : [],
-					"Report" : []
-				}
-			};
+angular
+		.module("dragger")
+		.controller(
+				"buildReportController",
+				function($scope, $http) {
+					$scope.models = {
+						selected : null,
+						lists : {
+							"Report" : []
+						}
+					};
 
-			$http({
-				method : 'GET',
-				url : '/api/queryColumns'
-			}).then(
-					function successCallback(response) {
-						angular.forEach(response.data._embedded.queryColumns,
-								function(column) {
-									$scope.models.lists.Columns.push(column);
-								});
-					});
-		});
+					$http({
+						method : 'GET',
+						url : '/api/querySources'
+					})
+							.then(
+									function successCallback(response) {
+										angular
+												.forEach(
+														response.data._embedded.querySources,
+														function(source) {
+															$scope.models.lists[source.name] = [];
+
+															$http(
+																	{
+																		method : 'GET',
+																		url : source._links.columns.href
+																	})
+																	.then(
+																			function successCallback(
+																					response) {
+																				angular
+																						.forEach(
+																								response.data._embedded.queryColumns,
+																								function(
+																										column) {
+																									$scope.models.lists[source.name]
+																											.push(column);
+																								});
+																			});
+														});
+									});
+
+				});
