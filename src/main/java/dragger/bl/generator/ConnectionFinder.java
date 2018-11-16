f266da66b15ad7e8ef5c43bb52623b5528265b79
OfekRv <ofekRv@gmail.com>
2018-11-16 22:41:00 +0200

    isAllSourcesCOnnected bug fixed

--- src/main/java/dragger/bl/generator/ConnectionFinder.java
+++ src/main/java/dragger/bl/generator/ConnectionFinder.java
@@ -8,31 +8,42 @@ import java.util.Map;
 import dragger.entities.QueryColumn;
 import dragger.entities.QuerySource;
 import dragger.entities.SourceConnection;
+import dragger.exceptions.DraggerConnectionException;
 
 public interface ConnectionFinder {
-	public default Collection<SourceConnection> findConnectionsBetweenSources(Collection<QuerySource> sources) {
+	public default Collection<SourceConnection> findConnectionsBetweenSources(Collection<QuerySource> sources)
+			throws DraggerConnectionException {
 		Collection<SourceConnection> connections = new ArrayList<>();
-
+		Collection<QuerySource> needToBeFoundSources = new ArrayList<>(sources);
 		for (QuerySource source : sources) {
 			for (Map.Entry<QuerySource, SourceConnection> connectoinEntry : getAllSourcesConnectedToSource(source)
 					.entrySet()) {
 				if (sources.contains(connectoinEntry.getKey())) {
 					connections.add(connectoinEntry.getValue());
+					needToBeFoundSources.remove(source);
 				}
 			}
 		}
+
 		// if we want exception when two sources used without connection
-		/*
-		 * if (needToBeFoundSources.size() > 0) { throw new
-		 * DraggerConnectionException("Could not find connection for source: " +
-		 * needToBeFoundSources); }
-		 */
+		if (needToBeFoundSources.size() > 0) {
+			throw new DraggerConnectionException("Could not find connection for source: " + needToBeFoundSources);
+		}
 
 		return connections;
 	}
 
 	public default boolean isAllSourcesConnected(Collection<QuerySource> sources) {
-		return findConnectionsBetweenSources(sources).size() == sources.size() || sources.size() <= 1;
+		if (sources.size() <= 1) {
+			return true;
+		}
+
+		try {
+			findConnectionsBetweenSources(sources);
+			return true;
+		} catch (DraggerConnectionException e) {
+			return false;
+		}
 	}
 
 	public default Map<QuerySource, SourceConnection> getAllSourcesConnectedToSource(QuerySource source) {
