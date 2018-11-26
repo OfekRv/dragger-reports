c5570443c777b3c288dce962b098d36d8a8d73a2
danielstrrr <43907815+danielstrrr@users.noreply.github.com>
2018-11-26 09:23:39 +0200

    Update dragger-framework.js

--- src/main/resources/static/dragger/framework/dragger-framework.js
+++ src/main/resources/static/dragger/framework/dragger-framework.js
@@ -33,7 +33,9 @@ angular
 									{
 										templateUrl : 'dragger/defineConnections/defineConnections-frame.html',
 										controller : 'defineConnectionsController'
-									})
+									}).otherwise({
+								redirectTo : '/generateReport'
+							})
 				})
 
 		.directive(
