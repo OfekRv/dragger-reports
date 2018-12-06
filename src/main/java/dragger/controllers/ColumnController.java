d1bcfb4437359c9871121d00cfa30d165c3f7124
OfekRv <ofekRv@gmail.com>
2018-12-06 09:07:46 +0200

    using JDBCType

--- src/main/java/dragger/controllers/ColumnController.java
+++ src/main/java/dragger/controllers/ColumnController.java
@@ -8,13 +8,11 @@ import java.util.Collection;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import dragger.entities.DataType;
-
 @RestController
 public class ColumnController {
 
 	@GetMapping("api/columns/availableDataTypes")
-	public Collection<DataType> getAvailableDataTypes() {
-		return asList(new DataType(JDBCType.NUMERIC), new DataType(JDBCType.VARCHAR), new DataType(JDBCType.DATE));
+	public Collection<JDBCType> getAvailableDataTypes() {
+		return asList(JDBCType.NUMERIC, JDBCType.VARCHAR, JDBCType.DATE);
 	}
 }
