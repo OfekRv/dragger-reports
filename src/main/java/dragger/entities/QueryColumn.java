668bcafc0cbc9277b0ccf3eea68c0a6387d66fed
Cusshel <40361210+Cusshel@users.noreply.github.com>
2018-12-06 17:56:34 +0200

    Update QueryColumn.java

--- src/main/java/dragger/entities/QueryColumn.java
+++ src/main/java/dragger/entities/QueryColumn.java
@@ -36,7 +36,7 @@ public class QueryColumn {
 	@Column(nullable = false)
 	private String name;
 
-	@Column(nullable = false)
+	@Column(nullable = false, length = 5000)
 	private String raw;
 
 	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH, optional = false)
