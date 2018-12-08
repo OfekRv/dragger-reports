283034e6ff53a06b1f1528de1338ba7491ec083d
Daniel <Daniel@Daniel-PC>
2018-12-08 23:37:26 +0200

    basic filtration added

--- src/main/java/dragger/controllers/ColumnController.java
+++ src/main/java/dragger/controllers/ColumnController.java
@@ -1,18 +1,18 @@
 package dragger.controllers;
 
 import static java.util.Arrays.asList;
-
-import java.sql.JDBCType;
 import java.util.Collection;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dragger.enums.DataType;
+
 @RestController
 public class ColumnController {
 
 	@GetMapping("api/columns/availableDataTypes")
-	public Collection<JDBCType> getAvailableDataTypes() {
-		return asList(JDBCType.NUMERIC, JDBCType.VARCHAR, JDBCType.DATE);
+	public Collection<DataType> getAvailableDataTypes() {
+		return asList(DataType.NUMBER,DataType.TEXT,DataType.DATE);
 	}
 }
--- src/main/resources/static/dragger/generateReport/generateReport.js
+++ src/main/resources/static/dragger/generateReport/generateReport.js
@@ -1,18 +1,124 @@
 angular.module("dragger").controller(
-		"generateReportController",
-		function($scope, $http) {
-			$scope.reports = [];
-			$scope.selected = null;
-			$scope.value = null;
+    "generateReportController",
+    function ($scope, $http) {
+        $scope.reports = [];
+        $scope.selectedReport = null;
 
-			$http({
-				method : 'GET',
-				url : '/api/reports'
-			}).then(
-					function successCallback(response) {
-						angular.forEach(response.data._embedded.reports,
-								function(report) {
-									$scope.reports.push(report);
-								});
-					});
-		});
+        $scope.filters = [];
+        $scope.operators = [];
+        // $scope.dataTypes = {"NUMERIC": "NUMBER", "VARCHAR": "TEXT", "DATE": "DATE"};
+        $scope.dataTypes = {};
+
+        $scope.filtered = "";
+
+        $http({
+            method: 'GET',
+            url: '/api/reports'
+        }).then(
+            function successCallback(response) {
+                angular.forEach(response.data._embedded.reports,
+                    function (report) {
+                        angular
+                            .forEach(
+                                report.query._links.columns,
+                                function (column) {
+                                    var columnDataPromise = $http(
+                                        {
+                                            method: 'GET',
+                                            url: column.href
+                                        }).then(
+                                        function successCallback(response) {
+                                            return response.data;
+                                        });
+                                    columnDataPromise.then(function (response) {
+                                        if (report.columns == undefined) {
+                                            report.columns = [];
+                                        }
+
+                                        report.columns.push(response);
+                                    })
+                                }, report);
+
+                        $scope.reports.push(report);
+                    });
+            });
+
+        $http({
+            method: 'GET',
+            url: '/api/columns/availableDataTypes'
+        }).then(
+            function successCallback(response) {
+                angular.forEach(response.data,
+                    function (dataType) {
+                        $scope.dataTypes[dataType.type] = dataType.name;
+                    });
+            });
+
+
+        $http({
+            method: 'GET',
+            url: '/api/filters'
+        }).then(
+            function successCallback(response) {
+                angular.forEach(response.data._embedded.filters,
+                    function (filter) {
+                        $scope.operators.push(filter)
+                    });
+            });
+
+        $scope.addFilter = function () {
+            $scope.filters.push({"value": null, "filter": null, "column": null});
+            $scope.filtered = "Filtered";
+        };
+
+        $scope.removeFilter = function (filterIndex) {
+            $scope.filters.splice(filterIndex, 1);
+            $scope.filtered = $scope.filters.length > 0 ? "Filtered" : "";
+        };
+
+        $scope.changeReport = function () {
+            $scope.filters = [];
+        }
+
+        $scope.downloadUrl = function () {
+            angular.forEach($scope.filters,
+                function (filter) {
+                    filter.columnId = filter.column.columnId;
+                    filter.filterId = filter.filter.id;
+                });
+
+            $http({
+                method: 'POST',
+                url: '/api/reports/generate' + $scope.filtered + 'Report?reportId=' + $scope.selectedReport.id,
+                data: $scope.filters,
+                responseType: 'arraybuffer'
+            }).success(function (data, status, headers) {
+                var headers = headers();
+
+                var fileNameHeader = headers['content-disposition'].split(';')[1].trim().split('=')[1];
+
+                var filename = fileNameHeader.replace(/"/g, '');
+                var contentType = headers['content-type'];
+
+                var linkElement = document.createElement('a');
+                try {
+                    var blob = new Blob([data], {type: contentType});
+                    var url = window.URL.createObjectURL(blob);
+
+                    linkElement.setAttribute('href', url);
+                    linkElement.setAttribute("download", filename);
+
+                    var clickEvent = new MouseEvent("click", {
+                        "view": window,
+                        "bubbles": true,
+                        "cancelable": false
+                    });
+                    linkElement.dispatchEvent(clickEvent);
+                } catch (ex) {
+                    console.log(ex);
+                }
+            }).error(function (data) {
+                console.log(data);
+            });
+        }
+    });
