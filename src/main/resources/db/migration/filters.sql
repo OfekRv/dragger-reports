469aa826a4935637876b65f368fd50479ae808be
OfekRv <ofekRv@gmail.com>
2018-12-11 13:04:23 +0200

    auto insrts filters to db

--- src/main/resources/db/migration/filters.sql
+++
@@ -1,3 +0,0 @@
-insert into drg_filters (id, name, raw_filter) values (1, 'גדול מ', '<');
-insert into drg_filters (id, name, raw_filter) values (2, 'קטן מ', '>');
-insert into drg_filters (id, name, raw_filter) values (3, 'שווה ל', '=');
