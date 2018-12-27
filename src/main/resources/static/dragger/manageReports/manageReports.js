30fd1230e35dbebaa51801f29c56317a4a4ea109
BuildTools <unconfigured@null.spigotmc.org>
2018-12-27 14:28:49 +0200

    manageReports

---
+++ src/main/resources/static/dragger/manageReports/manageReports.js
@@ -0,0 +1,40 @@
+angular
+		.module("dragger")
+		.controller(
+				"manageReportsController",
+				function($scope, $http) {
+					$scope.dropCallback = function(index, item) {
+					if(confirm("את/ה בטוח שברצונך למחוק את הדוח?"))
+                    {
+                            $http({
+                                method : 'DELETE',
+                                url : '/api/reports/'+ $scope.models.lists['Reports'][index].id
+                            }).then(function(){
+                                $scope.initialize();
+                            });
+                        };
+                    }
+
+                    $scope.initialize = function(){
+                        $scope.models = {
+                            selected : null,
+                            lists : {
+                                "Reports" : []
+                            }
+                        };
+                        $http({
+                            method : 'GET',
+                            url : '/api/reports'
+                        }).then(
+                                function successCallback(response) {
+                                    angular.forEach(
+                                            response.data._embedded.reports,
+                                            function(report) {
+                                                $scope.models.lists['Reports']
+                                                        .push(report);
+                                            });
+                                });
+					}
+
+					$scope.initialize();
+				});
