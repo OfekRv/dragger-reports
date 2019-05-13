baee440523de76fbcb4483f9d9533179b0cd0d2d
OfekRv <ofekrv@gmail.com>
2019-05-13 17:50:22 +0300

    Update DashboardController.java

--- src/main/java/dragger/controllers/DashboardController.java
+++ src/main/java/dragger/controllers/DashboardController.java
@@ -1,23 +1,58 @@
 package dragger.controllers;
 
-import dragger.contracts.ChartToDashboardContract;
-import dragger.repositories.ChartRepository;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import dragger.entities.Chart;
 import dragger.entities.Dashboard;
 import dragger.exceptions.DraggerException;
+import dragger.repositories.ChartRepository;
+import dragger.repositories.DashboardRepository;
 
 @RestController
 public class DashboardController {
-
 	@Autowired
 	private ChartRepository chartRepository;
+	@Autowired
+	private DashboardRepository dashboardRepository;
 
-	@DeleteMapping("api/dashboard/removeChart")
-	public void removeChartFromDashboard(@RequestBody Dashboard dashboard, @RequestBody Chart chartToRemove)
-			throws DraggerException {
+	@GetMapping("api/dashboard/{dashboardId}/removeChart/{chartId}")
+	public void removeChartFromDashboard(@PathVariable("dashboardId") long dashboardId,
+			@PathVariable("chartId") long chartId) throws DraggerException {
+		Dashboard dashboard = findDashboardById(dashboardId);
+		Chart chartToRemove = findChartById(chartId);
 		dashboard.getCharts().remove(chartToRemove);
+	}
+
+	@GetMapping("api/dashboard/{dashboardId}/addChart/{chartId}")
+	public void addChartToDashboard(@PathVariable("dashboardId") long dashboardId,
+			@PathVariable("chartId") long chartId) throws DraggerException {
+		Dashboard dashboard = findDashboardById(dashboardId);
+		Chart chartToAdd = findChartById(chartId);
+		dashboard.getCharts().add(chartToAdd);
+	}
+
+	private Dashboard findDashboardById(long dashboardId) throws DraggerException {
+		Optional<Dashboard> requestedDashboard = dashboardRepository.findById(dashboardId);
+
+		if (requestedDashboard.isPresent()) {
+			return requestedDashboard.get();
+		}
+
+		throw new DraggerException("Dashboard id:" + dashboardId + " not found");
+	}
+
+	private Chart findChartById(long chartId) throws DraggerException {
+		Optional<Chart> requestedCHart = chartRepository.findById(chartId);
+
+		if (requestedCHart.isPresent()) {
+			return requestedCHart.get();
+		}
+
+		throw new DraggerException("Chart id:" + chartId + " not found");
 	}
 }
